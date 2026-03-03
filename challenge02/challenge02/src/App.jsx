ea9d76b05df26e41dc6304a39a43b34e92ea45d2
juan miguel <juan_miguel.perdomo@uao.edu.co>
2026-03-03 10:49:19 -0500

    challenge 02

---
+++ challenge02/challenge02/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react'
+
+import './App.css'
+import ContactsForm from './components/ContactsForm'
+import ContactsList from './components/ContactsList'
+
+function App() {
+  const [contacts,setContacts] = useState([])
+  const [load,setLoad] = useState(true)
+
+  useEffect(()=>{
+
+    const timer = setTimeout(() => {
+          setContacts([
+            {name: "Juan", phone: "3160508012"}, 
+            {name: "Andrea", phone: "3045792069"}
+          ])
+          setLoad(false)
+    }, 1500)
+
+    return()=>{
+      clearInterval(timer)
+      console.log("intervalo limpiado")
+    }
+  },[])
+
+  const addContact = (contact) =>{
+    setContacts(prev => [...prev, contact])
+  }
+
+  const deleteContact = (index) =>{
+    setContacts(prev => prev.filter((_, i) => i !== index))
+  }
+
+  return(
+    <div>
+      <h1>AGENDA DE CONTACTOS</h1>
+      {load?(
+        <p>Cargando contactos...</p>
+      ):(
+        <>
+          <ContactsForm addContact={addContact}/>
+          <ContactsList contacts={contacts} deleteContact={deleteContact}/>        
+        </>
+      )}
+    </div>
+  )
+}
+
+
+
+
+export default App
