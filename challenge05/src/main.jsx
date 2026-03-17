cfe6e6cc90478d2c4856849c0ddc790851c803cf
juan miguel <juan_miguel.perdomo@uao.edu.co>
2026-03-16 20:35:24 -0500

    style

--- challenge05/src/main.jsx
+++ challenge05/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './App.css'
 import Row from './components/QAtm'
 
 createRoot(document.getElementById('root')).render(
